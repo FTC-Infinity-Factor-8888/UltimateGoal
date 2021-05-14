1f9ec2805f1ec672844c976895342f6e48509530
VanFam <william.vanuitert@gmail.com>
2021-05-14 14:27:07 -0400

    Finished writing a new drive program. The robot will not run it however. The drive function is being called from FLLDriveTest. We are getting a nullPointerException. David, any ideas?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FLLDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FLLDrive.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
@@ -14,9 +15,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
-public class FLLDrive{
+public class FLLDrive {
     private final static double MAX_ROBOT_SPEED = 1.0;
     private final static double MIN_ROBOT_SPEED = 0.3;
+    private int maximumRobotTps = 2500;
 
     private UltimateGoalRobot creator;
     private Telemetry telemetry;
@@ -53,6 +55,49 @@ public class FLLDrive{
         lrMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rfMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rrMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    private void powerTheWheels(double lfPower, double lrPower, double rfPower, double rrPower) {
+        double leftMax = Math.max(Math.abs(lfPower), Math.abs(lrPower));
+        double rightMax = Math.max(Math.abs(rfPower), Math.abs(rrPower));
+        double max = Math.max (leftMax, rightMax);
+
+        if(max > MAX_ROBOT_SPEED) {
+            lfPower /= max;
+            lrPower /= max;
+            rfPower /= max;
+            rrPower /= max;
+        }
+
+        if (lfMotor.getMode().equals(DcMotor.RunMode.RUN_USING_ENCODER)) {
+            double lfVelocity = lfPower * maximumRobotTps;
+            double lrVelocity = lrPower * maximumRobotTps;
+            double rfVelocity = rfPower * maximumRobotTps;
+            double rrVelocity = rrPower * maximumRobotTps;
+
+            if (creator.opModeIsActive()) {
+
+                lfMotor.setVelocity(lfVelocity);
+                lrMotor.setVelocity(lrVelocity);
+                rfMotor.setVelocity(rfVelocity);
+                rrMotor.setVelocity(rrVelocity);
+            }
+            else {
+                throw new EmergencyStopException("PowerTheWheels");
+            }
+        }
+        else {
+            // We assume that we will be using RUN_TO_POSITION mode.
+            if(creator.opModeIsActive()) {
+                lfMotor.setPower(lfPower);
+                lrMotor.setPower(lrPower);
+                rfMotor.setPower(rfPower);
+                rrMotor.setPower(rrPower);
+            }
+            else {
+                throw new EmergencyStopException("PowerTheWheels");
+            }
+        }
     }
 
     private void initializeIMU() {
@@ -112,38 +157,71 @@ public class FLLDrive{
 
     private void resetMotors() {
         setMotorMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        setMotorMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
-    private double getMotorPosition() {`
+    private double getMotorPosition() {
         double lfPosition = lfMotor.getCurrentPosition();
         double rfPosition = rfMotor.getCurrentPosition();
         double lrPosition = lrMotor.getCurrentPosition();
         double rrPosition = rrMotor.getCurrentPosition();
 
-        return (lfPosition + rfPosition + lrPosition + rrPosition) / 4;
+        double motorPositionAverage = (lfPosition + rfPosition + lrPosition + rrPosition) / 4;
+
+        return motorPositionAverage * ticksPerInch;
     }
 
-    public void fllDrive(double distance) {
+    public void drive(double distance) {
         double desiredHeading = getImuHeading();
         setMotorDistanceToTravel(distance, new int[]{1, 1, 1, 1});
 
-        double accelDecelInches;
+        double accelInches;
+        double decelInches;
 
         double halfSlope = MAX_ROBOT_SPEED - MIN_ROBOT_SPEED;
 
         if (distance / 8 < 2) {
-            accelDecelInches = 2;
+            accelInches = 2;
         }
         else {
-            accelDecelInches = distance / 8;
+            accelInches = distance / 8;
         }
 
-        double wholeSlope = halfSlope / accelDecelInches;
+        if (distance / 6 < 3) {
+            decelInches = 3;
+        }
+        else {
+            decelInches = distance / 6;
+        }
+
+        double wholeAccelSlope = halfSlope / accelInches;
+        double wholeDecelSlope = -halfSlope / decelInches;
 
         resetMotors();
         while (creator.opModeIsActive() && lfMotor.isBusy() && rfMotor.isBusy() && lrMotor.isBusy()
-                && rrMotor.isBusy()) {
+                && rrMotor.isBusy() && getMotorPosition() <= accelInches) {
 
+            double acceleration = wholeAccelSlope * getMotorPosition();
+            powerTheWheels(acceleration, acceleration, acceleration, acceleration);
         }
+        while (creator.opModeIsActive() && lfMotor.isBusy() && rfMotor.isBusy() && lrMotor.isBusy()
+                && rrMotor.isBusy() && getMotorPosition() <= distance - accelInches - decelInches) {
+
+            powerTheWheels(MAX_ROBOT_SPEED, MAX_ROBOT_SPEED, MAX_ROBOT_SPEED, MAX_ROBOT_SPEED);
+        }
+        resetMotors();
+        while (creator.opModeIsActive() && lfMotor.isBusy() && rfMotor.isBusy() && lrMotor.isBusy()
+                && rrMotor.isBusy() && getMotorPosition() <= distance) {
+
+            double deceleration = wholeDecelSlope * getMotorPosition();
+            powerTheWheels(deceleration, deceleration, deceleration, deceleration);
+        }
+
+        if (!creator.opModeIsActive()) {
+            throw new EmergencyStopException("FLLDrive");
+        }
+        setMotorMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        powerTheWheels(0, 0, 0, 0);
+
     }
 }
